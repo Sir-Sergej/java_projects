5526313c8f4837745c0229e3750ca74418bf8c83
Sir-sergej <sheryaevkj@mail.ru>
2021-08-13 17:34:09 +0300

    version v6

--- java_projects/src/com/company/file/FileWrite.java
+++ java_projects/src/com/company/file/FileWrite.java
@@ -6,14 +6,21 @@ public class FileWrite {
 
     public static void main(String[] args) throws IOException {
 
-        try (PrintStream file = new PrintStream("/home/sergey/dataforjava/data.txt")) {
+        File file = new File("/home/sergey/dataforjava/data.txt");
+        try (PrintStream printstream = new PrintStream(file)) {
 
             for(int i = 0; i < 1000; i++) {
-                file.print(i);
+                printstream.print(i);
             }
 
         } catch (FileNotFoundException exp) {
             System.out.println("file not found");
+        }
+
+        FileReader reader = new FileReader(file);
+        try (BufferedReader br = new BufferedReader(reader)) {
+            String line = br.readLine();
+            System.out.println(line + '\n');
         }
     }
 }
